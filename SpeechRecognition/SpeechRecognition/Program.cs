0e51ac220bf9a00aa4b933ef0995f201eda13f77
Sindre Hansen <SindreRynning.Hansen@nov.com>
2016-12-20 09:58:10 +0100

    Adding SpeechSynthesizer

--- SpeechRecognition/SpeechRecognition/Program.cs
+++ SpeechRecognition/SpeechRecognition/Program.cs
@@ -11,19 +11,27 @@ namespace SpeechRecognition
 
     class Program
     {
-        
+        private static bool run = false;
+        private static SpeechRecognitionEngine sr = new SpeechRecognitionEngine();
+        private static SpeechSynthesizer synth = new SpeechSynthesizer();
         static void Main(string[] args)
         {
-            SpeechRecognitionEngine sr = new SpeechRecognitionEngine();
-            intiSpeetch(sr);
+            intiSpeech();
             sr.RecognizeAsync(RecognizeMode.Multiple);
+            initSpeechSynt();
             Console.ReadLine();
         }
 
-        private static void intiSpeetch(SpeechRecognitionEngine sr)
+        private static void initSpeechSynt()
+        {
+            synth.SetOutputToDefaultAudioDevice();
+            synth.Speak("Speech Synthesizer enabled");
+        }
+
+        private static void intiSpeech()
         {
             Choices colors = new Choices();
-            colors.Add(new string[] { "start", "stop", "system" });
+            colors.Add(new string[] { "Exit program", "Start", "Stop" });
 
             GrammarBuilder gb = new GrammarBuilder();
             gb.Append(colors);
@@ -37,7 +45,35 @@ namespace SpeechRecognition
 
         private static void sr_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
+            
             Console.WriteLine("You said to me: " + e.Result.Text);
+            synth.Speak(e.Result.Text);
+            switch (e.Result.Text)
+            {
+                case "Start":
+                    run = true;
+                    Task.Factory.StartNew(() => DoSome());
+                    break;
+                case "Stop":
+                    run = false;
+                    break;
+
+                case "Exit program":
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        private static void DoSome()
+        {
+            int i = 0;
+            while (run)
+            {
+                Console.WriteLine("Conting.. " + i);
+                i++;
+                System.Threading.Thread.Sleep(250);
+            }
+             
         }
     }
 }
